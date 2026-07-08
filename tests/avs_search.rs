c9d7b33abf0b5ab69d0cd3930c811e145279441d
silent9669 <phucchemistry69@gmail.com>
2026-07-08 16:31:20 +0700

    chore: remove scratch test files

--- tests/avs_search.rs
+++
@@ -1,8 +0,0 @@
-#[tokio::test]
-async fn test_avs_search() {
-    let provider = ani_desk_core::providers::animevietsub::AnimeVietSubProvider::new();
-    let res = ani_desk_core::providers::AnimeProvider::search(&provider, "one piece")
-        .await
-        .unwrap();
-    println!("Search results: {:?}", res);
-}
--- tests/test_mb_get.rs
+++
@@ -1,9 +0,0 @@
-use ani_desk_core::providers::AnimeProvider;
-
-#[tokio::test]
-async fn test_mb_get() {
-    let provider = ani_desk_core::providers::moviebox::MovieBoxProvider::new();
-    // Test a GET request to see if it works. "35142" is a dummy ID.
-    let res = provider.get_anime_details("35142").await;
-    println!("Details result: {:?}", res);
-}
